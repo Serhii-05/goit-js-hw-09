63cfba060c484c00041f4aeff1eca8af1bd374fa
Serhii Melnychenko <higankingpro@gmail.com>
2022-04-05 14:25:54 +0300

    function getRandom+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const isDisabled = true;
+let timeIntervalId = 0;
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+buttonStop.disabled = isDisabled;
+
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
 
